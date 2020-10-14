edd48b95891d6838196062033f874ede2c56d617
Premraj01 <premrajtripute777@gmail.com>
2020-10-14 12:36:51 +0530

    Twitter_Crawler

---
+++ keys.py
@@ -0,0 +1,9 @@
+consumer_key = "vDskCByhRtdLTXh3kVmHX29Ut"
+consumer_secret = "62O7Yv1XnepxZhjckRnB0RiF19SVWgxyODrntXkdL3p0tF0eti"
+access_token = "1253039170316099588-MHH8FBF1EVZT1QNjbhX9Yb27x6zOYd"
+access_token_secret = "riSXX9xmPJ7Qy0qfVSu6f233G3yQv9EbAi3JXIJPxLEOS"
+
+
+
+
+
